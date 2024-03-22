1fba3469803449238afdadcc9273718ede367deb
lab_services_student <lab_services_student@labVMH8OX>
2024-03-22 15:33:39 +0200

    Add project files.

---
+++ Controllers/userController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class userController : Controller
+    {
+        public userTable usrtbl=new userTable();
+
+
+        
+        [HttpPost]
+        public ActionResult About(userTable Users)
+        {
+            var result = usrtbl.insert_User(Users);
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+        public ActionResult About()
+        {
+            return View(usrtbl);
+        }
+    }
+}
---
+++ Models/userTable.cs
@@ -0,0 +1,47 @@
+﻿using System.Data.SqlClient;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Models
+{
+    public class userTable : Controller
+    {
+        public static string con_string = "Server=tcp:sqlexample.database.windows.net,1433;Initial Catalog=sqlexampleD;Persist Security Info=False;User ID=Phenyo;Password=Birdzone33;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30";
+
+        public static SqlConnection con = new SqlConnection(con_string);
+
+
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public string Email {  get; set; }
+
+
+
+
+        public int insert_User(userTable n)
+        {
+
+            try
+            {
+                string sql = "Insert into userTable (userName, userSurname, userEmail) VALUES (@Name, @Surname, @Email";
+                SqlCommand cmd = new SqlCommand(sql, con);
+                cmd.Parameters.AddWithValue("@Name", n.Name);
+                cmd.Parameters.AddWithValue("@Surname", n.Surname);
+                cmd.Parameters.AddWithValue("@Email", n.Email);
+                con.Open();
+                int rowsAffected = cmd.ExecuteNonQuery();
+                con.Close();
+                return rowsAffected;
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+
+        }
+
+
+
+    }
+}
